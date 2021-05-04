221a314dc94f494de6d8ed7ceaf3ad2e9cea745c
Mir Mohit <42828066+MirMohit@users.noreply.github.com>
2021-05-04 09:01:47 +0600

    Add files via upload

---
+++ app/app/src/main/java/com/example/fbclick/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.fbclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button fbClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fbClick = findViewById(R.id.fbClick);
+
+        fbClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotUrl("https://facebook.com/groups/hungryma");
+            }
+        });
+
+
+    }
+
+    private void gotUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
+}
